5aaf10c26e12b7353bcd56a25481515389011f89
Marc Enschede <marc@marcenschede.nl>
2018-06-03 17:25:48 +0200

    Add Jenkinsfile

--- src/main/kotlin/nl/marcenschede/tests/pipeline/PipelineApplication.kt
+++ src/main/kotlin/nl/marcenschede/tests/pipeline/PipelineApplication.kt
@@ -5,6 +5,8 @@ import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+
+
 @SpringBootApplication
 @RestController
 class PipelineApplication {

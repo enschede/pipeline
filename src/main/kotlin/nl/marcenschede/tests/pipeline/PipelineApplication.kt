f341876bbd886bb39e60254cffbaa894a8cdf918
Marc Enschede <marc@marcenschede.nl>
2018-06-03 14:58:27 +0200

    Initial Spring Boot app in Kotlin

---
+++ src/main/kotlin/nl/marcenschede/tests/pipeline/PipelineApplication.kt
@@ -0,0 +1,20 @@
+package nl.marcenschede.tests.pipeline
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+class PipelineApplication {
+
+    @GetMapping("/hello")
+    fun rootGet(): String {
+        return "Hello hackers!"
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<PipelineApplication>(*args)
+}
